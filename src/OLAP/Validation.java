5ed04546105e82bbf3ff179ad37f5343ef731009
JonasTan <JonasTan2015@hotmail.com>
2017-04-16 20:25:56 -0400

    Add validation class. It could validate select, group by, grouping variables, functions. But not having clause

--- src/OLAP/Validation.java
+++ src/OLAP/Validation.java
@@ -3,6 +3,8 @@ package OLAP;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.regex.*;
 
 public class Validation {
 	//List of projected attributes for the query output
@@ -25,6 +27,16 @@ public class Validation {
 		
 		private HashMap<String, HashMap<String, Integer>> outermap = null;
 		
+		private static HashSet<String> aggregate_set = new HashSet<String>(){{
+																		add("sum");
+																		add("avg");
+																		add("max");
+																		add("min");
+																		add("count");
+																}};
+		
+																
+																
 		public Validation(){
 			
 			this.S = new ArrayList<String>(Arrays.asList(MFConfig.S)); // select
@@ -34,5 +46,65 @@ public class Validation {
 			this.O = new ArrayList<String>(Arrays.asList(MFConfig.O)); // such that
 			this.G = MFConfig.G;
 			
+		}
+		
+		
+		public boolean isValid() throws Exception{
+			try {
+				isValidSelect();
+				isValidFunctionVariables();
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				throw new Exception(e.getMessage());
+			}
+			
+			return true;
+		}
+		
+		
+		private boolean isValidSelect() throws Exception{
+			int max_gv = -1;
+			for(String str : S){
+				if(!V.contains(str) && !F.contains(str)){
+					throw new Exception("Illegal attribute in select statement ");
+				}
+				
+				if(F.contains(str)){
+					String group_ID = str.split("_")[0];
+					if(Pattern.matches("[0-9]+", group_ID)){
+						max_gv = Math.max(max_gv, Integer.parseInt(group_ID));
+					}
+				}
+			}
+			
+			if(max_gv > N)
+				throw new Exception("In your functions, there is no grouping variable " + max_gv);
+			
+			return true;
+		}
+		
+		private boolean isValidFunctionVariables() throws Exception{
+			
+			//Traverse the Functions, check if all the aggregate functions are valid. Find the maximum ID of grouping variables
+			int max_gv = -1;
+			for(String func : F){
+				String[] attr = func.split("_");
+				if(Pattern.matches("[0-9]+", attr[0])){
+					max_gv = Math.max(max_gv, Integer.parseInt(attr[0]));
+					if(! aggregate_set.contains(attr[1])){
+						throw new Exception("Wrong aggregate function in" + func);
+					}
+				}else{
+					if(! aggregate_set.contains(attr[0])){
+						throw new Exception("Wrong aggregate function in" + func);
+					}
+				}
+			}
+			
+			//If the maximum ID of grouping variable exceeds N, throw an exception
+			if(max_gv > N)
+				throw new Exception("In your functions, there is no grouping variable " + max_gv);
+			
+			return true;
 		}
 }
